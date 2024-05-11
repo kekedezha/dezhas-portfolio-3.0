7bba8b737f917bd9f65fd13ced879a75c1d6171c
kekedezha <dezhac6@gmail.com>
2024-05-11 11:41:31 -0400

    updated image path

--- app/extra/shoes.ts
+++ app/extra/shoes.ts
@@ -1,42 +1,39 @@
-import Rad from '../../public/01_RAD_DROP_16_LIGHT_GREY_SIDE_140x@3x.webp';
-import Norda from '../../public/NDA-THE-001-WHITEGUMSIDER.webp';
-import Asics from '../../public/ASICS_GEL_NIMBUS_26.webp';
-import Ambush from '../../public/af1_ambush.webp';
 import Lost_Found from '../../public/aj1_lost&found.webp';
 import WashedBlack from '../../public/aj1_washedblack.webp'
-export const shoes = [
+
+export const SHOES = [
     {
-        src: {Norda},
+        src: '/NDA-THE-001-WHITEGUMSIDER.webp',
         alt: "Norda 001 trail running shoe",
         title: "Norda 001 - White Gum",
         purpose: "Trail Running"
     },
     {
-        src: {Asics},
+        src: '/ASICS_GEL_NIMBUS_26.png',
         alt: "Asics Gel-Nimbus 26 road running shoes.",
         title: "Asics Gel-Nimbus 26",
         purpose: "Road Running"
     },
     {
-        src: {Rad},
+        src: '/01_RAD_DROP_16_LIGHT_GREY_SIDE_140x@3x.webp',
         alt: "R.A.D. One Oatmeal Cross-training shoes.",
         title: "R.A.D. One Oatmeal",
         purpose: "Cross-training"
     },
     {
-        src: {Ambush},
+        src: '/af1_ambush.png',
         alt: "Air Force 1 - Ambush",
         title: "Air Force 1 - Ambush",
         purpose: "Casual"
     },
     {
-        src: {Lost_Found},
+        src: '/aj1_lost&found.png',
         alt: "Air Jordan 1 High - Lost & Found",
         title: "Air Jordan 1 High - Lost & Found",
         purpose: "Casual"
     },
     {
-        src: {WashedBlack},
+        src: '/aj1_washedblack.png',
         alt: "Air Jordan 1 High - Washed Black",
         title: "Air Jordan 1 High - Washed Black",
         purpose: "Casual"
