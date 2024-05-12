bd8b6e0851ab3bac83976478b69919b06f0b1b83
kekedezha <dezhac6@gmail.com>
2024-05-12 12:42:31 -0400

    updated carousel array prop type

--- app/components/EmblaCarousel.tsx
+++ app/components/EmblaCarousel.tsx
@@ -7,14 +7,17 @@ import {
   PrevButton,
   usePrevNextButtons
 } from './EmblaCarouselArrowButtons'
+import Image from 'next/image'
+
 
 type PropType = {
-  slides: number[]
-  options?: EmblaOptionsType
+  slides: any[]
+  options?: EmblaOptionsType,
+  strava: boolean
 }
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props
+  const { slides, options, strava } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [
     Autoplay({ playOnInit: false, delay: 3000 })
   ])
@@ -66,10 +69,27 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map((index) => (
+          {slides.map((slide, index) => (
             <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">
-                <span>{index + 1}</span>
+              <div className="embla__slide__number text-center flex flex-col justify-center">
+                { strava ? (
+                  <p>Hello</p>
+                )
+                : 
+                (
+                  <div className="embla__slide__number text-center flex flex-col justify-center">
+                    <Image 
+                      src={slide.src}
+                      alt={slide.alt}
+                      width="250"
+                      height="50"
+                      className="rounded-lg object-cover object-[-16px] sm:object-center"
+                    />
+                    <p className="mt-2 text-base text-zinc-300 w-full tracking-tighter" >{slide.title}</p>
+                    <p className="text-sm text-zinc-300 w-full tracking-tighter" >{slide.purpose}</p>
+                  </div>
+                )
+                }
               </div>
             </div>
           ))}
