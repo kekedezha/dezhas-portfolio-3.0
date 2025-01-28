1f9b43be35213302d870e66decf7143606b174b1
kekedezha <dezhac6@gmail.com>
2025-01-28 17:36:54 -0500

    blog post incoming...

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -3,10 +3,95 @@ import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import { highlight } from 'sugar-high'
 
 function clsx(...args: any) {
 	return args.filter(Boolean).join(" ");
 }
+
+function Table({ data }) {
+	let headers = data.headers.map((header, index) => (
+		<th key={index}>{header}</th>
+	))
+	let rows = data.rows.map((row, index) => (
+		<tr key={index}>
+			{row.map((cell, cellIndex) => (
+				<td key={cellIndex}>{cell}</td>
+			))}
+		</tr>
+	))
+
+	return (
+		<table>
+			<thead>
+				<tr>{headers}</tr>
+			</thead>
+			<tbody>{rows}</tbody>
+		</table>
+	)
+}
+
+function CustomLink(props) {
+	let href = props.href
+
+	if (href.startsWith('/')) {
+		return (
+			<Link href={href} {...props}>
+				{props.children}
+			</Link>
+		)
+	}
+
+	if (href.startsWith('#')) {
+		return <a {...props} />
+	}
+
+	return <a target="_blank" rel="noopener noreferrer" {...props} />
+}
+
+function RoundedImage(props) {
+	return <Image alt={props.alt} className="rounded-lg" {...props} />
+}
+
+function Code({ children, ...props }) {
+	let codeHTML = highlight(children)
+	return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+}
+
+function slugify(str) {
+	return str
+		.toString()
+		.toLowerCase()
+		.trim() // Remove whitespace from both ends of a string
+		.replace(/\s+/g, '-') // Replace spaces with -
+		.replace(/&/g, '-and-') // Replace & with 'and'
+		.replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+		.replace(/\-\-+/g, '-') // Replace multiple - with single -
+}
+
+function createHeading(level) {
+	const Heading = ({ children }) => {
+		let slug = slugify(children)
+		return React.createElement(
+			`h${level}`,
+			{ id: slug },
+			[
+				React.createElement('a', {
+					href: `#${slug}`,
+					key: `link-${slug}`,
+					className: 'anchor',
+				}),
+			],
+			children
+		)
+	}
+
+	Heading.displayName = `Heading${level}`
+
+	return Heading
+}
+
 const components = {
 	h1: ({ className, ...props }) => (
 		<h1
@@ -175,4 +260,13 @@ export function Mdx({ code }: MdxProps) {
 			<Component components={components} />
 		</div>
 	);
+}
+
+export function CustomMDX(props) {
+	return (
+		<MDXRemote
+			{...props}
+			components={{ ...components, ...(props.components || {}) }}
+		/>
+	)
 }
